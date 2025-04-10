2a293d7a6f850656d9f615ffb57c51937291f55e
KangChanSong <thdrkdcks@naver.com>
2025-04-10 10:54:44 +0900

    [CI] 빌드용 커밋

--- src/main/java/com/gswagger/MatchnowSwaggerConstants.java
+++ src/main/java/com/gswagger/MatchnowSwaggerConstants.java
@@ -2,10 +2,16 @@ package com.gswagger;
 
 public class MatchnowSwaggerConstants {
     public static final String MATCHNOW_SWAGGER_ENABLED = "springdoc.matchnow-swagger.enabled";
+
     public static final String MATCHNOW_SWAGGER_CORS_ENABLED = "springdoc.matchnow-swagger.enable-cors";
+
     public static final String MATCHNOW_SWAGGER_PROPERTIES = "springdoc.matchnow-swagger";
+
     public static final String REDIRECT_PATH_REPLACER = "${springdoc.matchnow-swagger.redirect-path-replacer:defaultRedirectPathReplacer}";
+
     public static final String MATHCNOW_SWAGGER_PROPERTY_URL = "${springdoc.matchnow-swagger.property-url:/swagger-ui/properties}";
+
     public static final String MODEL_RESOLVER_OBJECT_MAPPER = "${springdoc.matchnow-swagger.object-mapper:jacksonObjectMapper}"; // jacksonObjectMapper -> spring boot default object mapper
+    
     public static final String USE_CUSTOM_OBJECT_MAPPER = "springdoc.matchnow-swagger.use-custom-object-mapper";
 }
